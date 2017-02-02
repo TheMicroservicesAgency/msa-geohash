f00095340a1175ab4a4b24ab8f671cfec4ace28f
Admin <admin@microservices.agency>
2017-02-01 21:25:20 -0500

    Updated the api to support geojson, precision decoding

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify, redirect, request
-import pygeohash as geohash
-from geojson import Point
+import pygeohash as pygeohash
+from geojson import Point, Polygon
 
 app = Flask(__name__)
+
 
 @app.route('/geohash/encode')
 def encode():
@@ -34,31 +35,102 @@ def encode():
         msg = {'ERROR': 'The specified longitude is invalid.'}
         return jsonify(msg), 400
 
-    result = geohash.encode(lat, lon)
+    result = pygeohash.encode(lat, lon)
 
     data = {'geohash': result}
     response = jsonify(data)
     return response
 
-@app.route('/geohash/decode')
-def decode():
-    ghash = request.args.get("h")
 
-    if ghash is None:
-        msg = {'ERROR': 'Missing geohash ! Try /decode?h=<geohash>'}
+@app.route('/geohash/decode/<geohash>')
+def decode(geohash):
+
+    if geohash is None:
+        msg = {'ERROR': 'Missing geohash ! Try /decode/<geohash>'}
         return jsonify(msg), 400
 
-    try:
-        result = geohash.decode(ghash)
-        #data = {'latitude': result[0], 'longitude': result[1]}
-        #response = jsonify(data)
-        response = jsonify(Point(result))
-        return response
+    exact_mode = request.args.get("exact")
+    if exact_mode:
 
-    except:
-        msg = {'ERROR': 'Could not decode the geohash'}
+        try:
+            # Returns four float values: latitude, longitude, the plus/minus error
+            # for latitude (as a positive number) and the plus/minus
+            # error for longitude (as a positive number).
+            lat, lon, lat_err, lon_err = pygeohash.decode_exactly(geohash)
+
+            print(lat)
+            print(lon)
+            print(lat_err)
+            print(lon_err)
+
+            # 1 ---- 2
+            # |      |
+            # 4 ---- 3
+            #
+            box1 = (lat - lat_err, lon + lon_err)
+            box2 = (lat + lat_err, lon + lon_err)
+            box3 = (lat + lat_err, lon - lon_err)
+            box4 = (lat - lat_err, lon - lon_err)
+
+            print(box1)
+            print(box2)
+            print(box3)
+            print(box4)
+
+            response = jsonify(Polygon([[box1, box2, box3, box4, box1]]))
+            return response
+
+        except:
+            msg = {'ERROR': 'Could not decode the geohash'}
+            return jsonify(msg), 400
+
+    else:
+
+        try:
+            # Returning two float with latitude and longitude
+            # containing only relevant digits and with trailing zeroes removed
+            result = pygeohash.decode(geohash)
+
+            # lat, lon to geojson point
+            response = jsonify(Point(result))
+            return response
+
+        except:
+            msg = {'ERROR': 'Could not decode the geohash'}
+            return jsonify(msg), 400
+
+
+@app.route('/geohash/precision/<geohash>')
+def precision(geohash):
+
+    if geohash is None:
+        msg = {'ERROR': 'Missing geohash ! Try /precision/<geohash>'}
         return jsonify(msg), 400
 
+    geohash_length = len(geohash)
+    if geohash_length > 10:
+        geohash_length = 10
+
+    # Precision lookup table for geohashes (in meters)
+    # from https://github.com/wdm0006/pygeohash/blob/master/pygeohash/distances.py
+    PRECISION = {
+        0: 20000000,
+        1: 5003530,
+        2: 625441,
+        3: 123264,
+        4: 19545,
+        5: 3803,
+        6: 610,
+        7: 118,
+        8: 19,
+        9: 3.71,
+        10: 0.6,
+    }
+
+    result = PRECISION[geohash_length]
+    data = {'precision' : { 'number': result, 'unit': 'meters'}}
+    response = jsonify(data)
+    return response
 
 if __name__ == "__main__":
 
